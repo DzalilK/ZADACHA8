e1f1a37a8f793f0f29fc76e7ad291046a25f109b
DzalilK <135230129+DzalilK@users.noreply.github.com>
2023-06-26 19:32:11 +0500

    REF

---
+++ REF/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2: Напешите программу,которая принимает на вход координаты двух точек и
+// находит расстояние между ними в 3D пространстве.
+
+ double Coord(int xA, int xB, int yA, int yB, int zA, int zB)
+ {
+    double distance = Math.Sqrt((xA - xB) * (xA - xB) + (yA - yB) * (yA-yB) + (zA + zB) * (zA -zB));
+    returen distance;
+ }
+
+
+Console.Write("Ввудите координату X для точки A: ");
+
+ 
